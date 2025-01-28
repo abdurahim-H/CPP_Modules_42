d69994328de8b393823a8f5b1cb8eae7aafa9a42
abdurahim-H <abul.hudul@gmail.com>
2025-01-28 10:56:05 +0000

    updated 'identify(Base& p' function

--- CPP_05-09/CPP_06/ex02/identify.cpp
+++ CPP_05-09/CPP_06/ex02/identify.cpp
@@ -1,9 +1,9 @@
 #include "A.hpp"
 #include "B.hpp"
-#include "C.hpp"  
+#include "C.hpp"	
 #include <iostream>
 
-void identify(Base* p) {  
+void identify(Base* p) {	
 	if (dynamic_cast<A*>(p) != NULL) {
 		std::cout << "A" << std::endl;
 	}
@@ -15,23 +15,42 @@ void identify(Base* p) {
 	}
 }
 
-void identify(Base& p) {
+// void identify(Base& p) {
+// 	try {
+// 		A& a = dynamic_cast<A&>(p);
+// 		(void)a;
+// 		std::cout << "A" << std::endl;
+// 		return;
+// 	} catch (...) {}
+// 	try {
+// 		B& b = dynamic_cast<B&>(p);
+// 		(void)b;
+// 		std::cout << "B" << std::endl;
+// 		return;
+// 	} catch (...) {}
+// 	try {
+// 		C& c = dynamic_cast<C&>(p);
+// 		(void)c;
+// 		std::cout << "C" << std::endl;
+// 		return;
+// 	} catch (...) {}
+// }
+
+void identify(Base& p) {	
 	try {
-		A& a = dynamic_cast<A&>(p);
-		(void)a;
+		(void)dynamic_cast<A&>(p);
 		std::cout << "A" << std::endl;
 		return;
 	} catch (...) {}
+
 	try {
-		B& b = dynamic_cast<B&>(p);
-		(void)b;
-		std::cout << "B" << std::endl;
-		return;
-	} catch (...) {}
+		(void)dynamic_cast<B&>(p);	
+		std::cout << "B" << std::endl;	
+		return;	
+	} catch (...) {}	
+
 	try {
-		C& c = dynamic_cast<C&>(p);
-		(void)c;
-		std::cout << "C" << std::endl;
-		return;
-	} catch (...) {}
-}
+		(void)dynamic_cast<C&>(p);	
+		std::cout << "C" << std::endl;	
+	} catch (...) {}	
+}	
